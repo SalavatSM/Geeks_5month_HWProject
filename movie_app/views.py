df8b375640005fa7a236d07cf6bab944e5dfb40c
SalavatSM <salavatfromkg@gmail.com>
2023-06-13 10:32:19 +0600

    Created Afisha project, add movie_app application, add Director, Movie, Review models. Add all models and models by one printing func.

---
+++ movie_app/views.py
@@ -0,0 +1,60 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
+from .models import Director, Movie, Review
+
+
+@api_view(['GET'])
+def director_list_api_view(request):
+    directors = Director.objects.all()
+    data = DirectorSerializer(instance=directors, many=True).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def director_item_api_view(request, id):
+    try:
+        director = Director.objects.get(id=id)
+    except Director.DoesNotExist:
+        return Response(data={'Director not found!'},
+                        status=status.HTTP_404_NOT_FOUND)
+    data = DirectorSerializer(director, many=False).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def movies_list_api_view(request):
+    movies = Movie.objects.all()
+    data = MovieSerializer(instance=movies, many=True).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def movies_item_api_view(request, id):
+    try:
+        movies = Movie.objects.get(id=id)
+    except Movie.DoesNotExist:
+        return Response(data={'Movie not found!'},
+                        status=status.HTTP_404_NOT_FOUND)
+    data = MovieSerializer(movies, many=False).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def reviews_list_api_view(request):
+    reviews = Review.objects.all()
+    data = ReviewSerializer(instance=reviews, many=True).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def reviews_item_api_view(request, id):
+    try:
+        reviews = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(data={'Review not found!'},
+                        status=status.HTTP_404_NOT_FOUND)
+    data = ReviewSerializer(reviews, many=False).data
+    return Response(data=data)
+
